add2abcd8d618d3ac2f4642a2fc1493c4997300b
gusta7w7 <162055858+gusta7w7@users.noreply.github.com>
2025-02-28 22:11:04 +0000

    Add files via upload

---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "PocketLua",
+  version = "1.0.0",
+  license = "MPL-2.0",
+  homepage = "https://github.com/gusta7w7/PocketLua",
+  description = "Run a MCPE 0.14.x server in Lua",
+  tags = { "PocketLua", "MCPE" },
+  author = { name = "Sr abelha" }
+}
